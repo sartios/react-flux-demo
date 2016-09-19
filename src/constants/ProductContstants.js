97b68595cb70d5a8c69ca7b12f081edf47be9529
sartios <sartios@hotmail.com>
2016-09-19 15:21:29 +0300

    add product constancs

---
+++ src/constants/ProductContstants.js
@@ -0,0 +1,9 @@
+var keyMirror = require('keyMirror');
+
+module.exports = keyMirror({
+	PRODUCT_CREATE: null,
+	PRODUCT_COMPLETE: null,
+	PRODUCT_DESTROY: null,
+	PRODUCT_DESTROY_COMPLETED: null,
+	PRODUCT_UPDATE: null
+});
